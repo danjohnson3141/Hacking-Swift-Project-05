6a625d2965145ac6414019e7150368f4c1408e0a
danjohnson3141 <drj3141@gmail.com>
2018-02-01 11:13:45 -0800

    Initial Commit

---
+++ Project5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Dan Johnson on 2/1/18.
+//  Copyright © 2018 Evanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
